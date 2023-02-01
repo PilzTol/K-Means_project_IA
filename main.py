9182d42fee00be68d7a88fd51c653bcf63afe748
PilzTol <wallissonvictor16@gmail.com>
2023-02-01 20:09:29 -0300

    quase pronto.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ k_centroides_aleatorios = int(input("Digite o número de grupos: "))
 gerar_centroide = k_centroides_aleatorios
 grupos = []
 mudanca = []
-k= 1
+novos_centros = []
 
 #Escolhe os centroides aleatoriamente.
 while gerar_centroide != 0:
@@ -27,30 +27,56 @@ for value in range(k_centroides_aleatorios):
      grupos.append([])
 
 #Onde a magia acontece
-
+verificacao = False
 ultimo_loop = True
 while ultimo_loop:
-    novos_grupos = calcular_distancias(grupos)
-    cont_1 = len(novos_grupos)
+    calcular_distancias(grupos)
+    cont_1 = len(grupos)
     cont_2 = cont_1
-    print(len(novos_grupos[0]))
-    print(len(novos_grupos[1]))
+    cont_3 = cont_2
     element = 0
 
-    for value in cont_2:
-        if novos_grupos[cont_2] == mudanca[cont_2]:
-            ultimo_loop = False
-        else:
-            ultimo_loop = True
-        cont_2 -=1
-
-    if ultimo_loop == False:
-        
-    for value in range(cont_1):
-        mudanca.append(len(novos_grupos[element]))
-        cont_1 -=1
+    for value in range(cont_3):
+        mudanca.append(len(grupos[element]))
+        cont_3 -=1
         element +=1
-    k -=1
-    cont_1 = 0 
+    cont_3 = cont_1
 
-    
+    for value in cont_3:
+        if grupos[cont_2-1] == mudanca[cont_2-1]:
+            ultimo_loop = True
+        else:
+            ultimo_loop = False
+            verificacao = True
+        cont_2 -=1
+        cont_3 -=1
+    #Calcula a média dos grupos e definir os novos pontos centrais
+        ##
+    #Atribuir os novos centros a var grupos para próxima iteração
+        grupos = novos_centros[:]
+        while verificacao:
+            calcular_distancias(grupos)
+            cont_1 = len(grupos)
+            cont_2 = cont_1
+            cont_3 = cont_2
+            element = 0
+
+            for value in range(cont_3):
+                mudanca.append(len(grupos[element]))
+                cont_3 -=1
+                element +=1
+            cont_3 = cont_1
+
+            for value in cont_3:
+                if grupos[cont_2-1] == mudanca[cont_2-1]:
+                    ultimo_loop = True
+                else:
+                    ultimo_loop = False
+                    verificacao = False
+                cont_2 -=1
+                cont_3 -=1
+            
+            #Calcula a média dos grupos e definir os novos pontos centrais
+                    ##
+    #Atribuir os novos centros a var grupos para próxima iteração
+        grupos = novos_centros[:]
