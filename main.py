844b0e2e7c5d77f554635eea1120fe760d44c98d
PilzTol <wallissonvictor16@gmail.com>
2023-02-05 11:56:22 -0300

    até que enfim

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ k_centroides_aleatorios = int(input("Digite o número de grupos: "))
 gerar_centroide = k_centroides_aleatorios
 grupos = []
 mudanca = []
+cont_6 = 2
 
 #Escolhe os centroides aleatoriamente.
 while gerar_centroide != 0:
@@ -28,6 +29,7 @@ for value in range(k_centroides_aleatorios):
 #Onde a magia acontece
 verificacao = False
 ultimo_loop = True
+ftime = False
 while ultimo_loop:
     novos_centros = []
     grupos = calcular_distancias()[:]
@@ -36,22 +38,25 @@ while ultimo_loop:
     cont_3 = cont_1
     element = 0
 
-
-    #Só falta mudar a parte da mudança, saber se houve mudança ou não nos grupos
+    cont_7 = 0
+    for var in grupos:
+        cont_7 += 1 
+        print(f"Grupo {cont_7}:{len(var)}")
+         
     for value in range(cont_3):
         mudanca.append(len(grupos[element]))
         cont_3 -=1
         element +=1
     cont_3 = cont_1
 
-    for value in range(cont_3):
-        if grupos[cont_2] == mudanca[cont_2]:
-            ultimo_loop = True
-        else:
-            verificacao = True
-        cont_2 +=1
-        cont_3 -=1
-
+    if ftime == True:
+        for value in range(cont_3):
+            if len(grupos[cont_2]) != mudanca[len(mudanca)-cont_6]: #só falta mexer aqui, 
+                ultimo_loop = True
+            else:
+                verificacao = True
+            cont_2 +=1
+            cont_3 -=1
     #Calcula a média dos grupos e definir os novos pontos centrais
     cont_4 = len(grupos[0][0])-1
     for grupo in grupos:
@@ -71,68 +76,67 @@ while ultimo_loop:
 
     with open('temporario.csv', 'r') as temporario_leitura_csv:
          novos_centros = list(csv.reader(temporario_leitura_csv))
+
     #Atribuir os novos centros a var grupos para próxima iteração
     with open('centros.csv', 'w', newline='') as novos_centros_csv:
         for element in novos_centros:
             writer = csv.writer(novos_centros_csv)
             writer.writerow(element)  
-    
     with open("temporario.csv", "w") as limpar_arquivo:
         limpar_arquivo.truncate() 
 
-    if True:
-        so_para = "fechar o arquivo"
+    ftime = True
+    while verificacao:
+        novos_centros = []
+        grupos = calcular_distancias()[:]
+        cont_1 = len(grupos)
+        cont_2 = 0
+        cont_3 = cont_1
+        element = 0
 
-#Copiar todo o while de cima no de baixo
-        while verificacao:
-            novos_centros = []
-            grupos = calcular_distancias()[:]
-            cont_1 = len(grupos)
-            cont_2 = cont_1
-            cont_3 = cont_2
-            element = 0
+        cont_7 = 0
+        for var in grupos:
+            cont_7 += 1 
+            print(f"Grupo {cont_7}:{len(var)}")
 
-            for value in range(cont_3):
-                mudanca.append(len(grupos[element]))
-                cont_3 -=1
-                element +=1
-            cont_3 = cont_1
+        for value in range(cont_3):
+            mudanca.append(len(grupos[element]))
+            cont_3 -=1
+            element +=1
+        cont_3 = cont_1
+        for value in range(cont_3):
+            if len(grupos[cont_2]) != mudanca[len(mudanca)-cont_6]: 
+                ultimo_loop = True
+            else:
+                ultimo_loop = False
+                verificacao = False
+            cont_2 +=1
+            cont_3 -=1
 
-            for value in range(cont_3):
-                if grupos[cont_2-1] == mudanca[cont_2-1]:
-                    ultimo_loop = True
-                else:
-                    ultimo_loop = False
-                    verificacao = False
-                    #Salva os grupos em um arquivo
+        #Calcula a média dos grupos e definir os novos pontos centrais
+        cont_4 = len(grupos[0][0])-1
+        for grupo in grupos:
+            cont_5 = 0
+            sublista = []
+            for value in range(cont_4):
+                total = 0
+                for element in grupo:
+                    total += float(element[cont_5])
+                total = total/len(grupo)
+                sublista.append(total)
+                cont_5 += 1	
+            with open('temporario.csv', 'a', newline='') as temporario_csv:
+                escrever_sublista = csv.writer(temporario_csv)
+                escrever_sublista.writerow(sublista)
+            sublista = []
 
-                cont_2 -=1
-                cont_3 -=1
+        with open('temporario.csv', 'r') as temporario_leitura_csv:
+            novos_centros = list(csv.reader(temporario_leitura_csv))
 
-            #Calcula a média dos grupos e definir os novos pontos centrais
-            cont_4 = len(grupos[0][0])-1
-            for grupo in grupos:
-                cont_5 = 0
-                sublista = []
-                for value in range(cont_4):
-                    total = 0
-                    for element in grupo:
-                        total += float(element[cont_5])
-                    total = total/len(grupo)
-                    sublista.append(total)
-                    cont_5 += 1	
-                with open('temporario.csv', 'a', newline='') as temporario_csv:
-                    escrever_sublista = csv.writer(temporario_csv)
-                    escrever_sublista.writerow(sublista)
-                sublista = []
-
-            with open('temporario.csv', 'r') as temporario_leitura_csv:
-                novos_centros = list(csv.reader(temporario_leitura_csv))
-            #Atribuir os novos centros a var grupos para próxima iteração
-            with open('centros.csv', 'w', newline='') as novos_centros_csv:
-                for element in novos_centros:
-                    writer = csv.writer(novos_centros_csv)
-                    writer.writerow(element)  
-    
-            with open("temporario.csv", "w") as limpar_arquivo:
-                limpar_arquivo.truncate() 
+        #Atribuir os novos centros a var grupos para próxima iteração
+        with open('centros.csv', 'w', newline='') as novos_centros_csv:
+            for element in novos_centros:
+                writer = csv.writer(novos_centros_csv)
+                writer.writerow(element)  
+        with open("temporario.csv", "w") as limpar_arquivo:
+            limpar_arquivo.truncate() 
